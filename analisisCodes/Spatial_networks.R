e3917be960bfeddedd7efcafacab4b18ce949188
EmilioLancis <emiliomora92@ciencias.unam.mx>
2023-06-13 13:37:53 -0600

    Aqui este codigo solo le cambie el spati net
    y entonces ahora si funciona el analisis de r3edes y ya quedaron las graficas
    
    ahora vamos a trabajar en la visualizacion para el siguie cokmmit

--- analisisCodes/Spatial_networks.R
+++ analisisCodes/Spatial_networks.R
@@ -1,23 +1,31 @@
 
 
+#this codes makes the analysis for the netwrks
+
 library(ggplot2)
 library(dplyr)
 library(tidyverse)
 library(igraph)
+library(reshape2)
 
 mycols <- c("#021128","#1b4a64", "#3585a0", "#759580", "#c78f34", "#fd9706","#fdb81c","#fbdb30")
 mycols3a <-c("#021128", "#fd9706", "#1b4a64", "#759580")
 mycols3b <-c("#1b4a64", "#fdb81c", "#759580")
 mycols3c <- c("#759580", "#1b4a64","#fdb81c")
+groupColors2 <- c("#fd9706", "#1b4a64",'#56B4E9', "#555555")
 
 
+#take the dataframe
 
 DF_TOTAL <- read.csv("archivosTrabajandose/toyModelHarvest/data/DF_total_TF.csv", header = TRUE)
 
+
+#remove unuses colunmns
 DF_TOTAL$X.1 <- NULL
 DF_TOTAL$FruitLoad <- NULL
 DF_TOTAL$TotalHarvest <- NULL
 
+#filter conditions that are unused 
 DF_TOTAL<- DF_TOTAL %>%
   filter(numWorkers != 5)%>%
   filter(numPlants != 4000)
@@ -30,6 +38,32 @@ DF_TOTAL<- DF_TOTAL %>%
 
 DF_TOTAL$porcionCosecha[DF_TOTAL$HarvestModel =="control"] <- 99
 
+################################
+#PARTE I
+#primero vamos a hacer las visualizaciones sin analisis
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 DF_NEW<-  DF_TOTAL%>%
   group_by(X, Y, Rep, numPlants)%>% #son las vairables que quedan y sobre esos escenarios, vamos a hacer las diferencias entre modelos
@@ -39,64 +73,56 @@ DF_NEW<-  DF_TOTAL%>%
   filter(NUEVA_RUST ==1)
 
 
-nP =1000
-rP = 0
-pC = 0.5
+#nP =5000
+#rP = 0
+#pC = 1
 
 
-columns = c("Rep", "numPlants",  "porcionCosecha",  "N_REDES",  "T_REDES" , "MAX_T_REDES") 
+#columns = c("Rep", "numPlants",  "porcionCosecha",  "N_REDES",  "T_REDES" , "MAX_T_REDES") 
+columns = c("Rep", "numPlants",  "porcionCosecha") 
+
 DF_POST = data.frame(matrix(nrow = 0, ncol = length(columns))) 
 colnames(DF_POST) = columns
 
-
-for (nP in unique(DF_NEW$numPlants)){
-  for (rP in unique(DF_NEW$Rep)) {
+  for (nP in unique(DF_NEW$numPlants)){
     for (pC in unique(DF_NEW$porcionCosecha)) {
-      
+      contador <- 0
+      for (rP in unique(DF_NEW$Rep)) {
       print(c(nP, rP, pC))
-    
-    DF_TEMP <- DF_NEW %>%
+      DF_TEMP <- DF_NEW %>%
       filter(numPlants ==nP & Rep == rP & porcionCosecha== pC)
-    
-    if (nrow(DF_TEMP)==0){  #esto porque hay comb que no exiten en las nuevas redes
+        if (nrow(DF_TEMP)==0){  #esto porque hay comb que no exiten en las nuevas redes
     }
-    else
+        else
     {  
-    N <- dim(DF_TEMP)[1]
-    
-
-    distance_matrix <- matrix(0,nrow=N,ncol=N)
-    for( i in 1:N){
-      for( j in 1:N ){
-        distance_matrix[i,j] <- sqrt( (DF_TEMP$X[i] - DF_TEMP$X[j])^2 + (DF_TEMP$Y[i] - DF_TEMP$Y[j])^2 )
+          N <- dim(DF_TEMP)[1]
+  
+          distance_matrix <- matrix(0,nrow=N,ncol=N)
+          for( i in 1:N){
+            for( j in 1:N ){
+              distance_matrix[i,j] <- sqrt( (DF_TEMP$X[i] - DF_TEMP$X[j])^2 + (DF_TEMP$Y[i] - DF_TEMP$Y[j])^2 )
         #image(distance_matrix)
-        
-        
         }
     }
     
-    Crit_Dist <- 1.5
+          Crit_Dist <- 1.5
     
     #MAKE ADJ MATRIX WITH CRITICAL DISTANCE
-    adj_matrix <- matrix(0,nrow=N,ncol=N)
+          adj_matrix <- matrix(0,nrow=N,ncol=N)
     
-    for(i in 1:N){
-      for(j in 1:N){
-        if(i != j & distance_matrix[i,j] <= Crit_Dist){
-          adj_matrix[i,j] = 1
-        }else{
-          adj_matrix[i,j] = 0
+          for(i in 1:N){
+            for(j in 1:N){
+              if(i != j & distance_matrix[i,j] <= Crit_Dist){
+                adj_matrix[i,j] = 1
+                }else{
+                  adj_matrix[i,j] = 0
         }
       }
     }
-    
    # image(adj_matrix)
-    
-    
-    plot_graph <- graph.adjacency(adj_matrix,mode="undirected")
+    #plot_graph <- graph.adjacency(adj_matrix,mode="undirected")
     #Pull out cluster IDs and put them in the data set
     groups <- unlist(clusters(plot_graph)[1])
-    
     DF_TEMP$cluster <- groups
     #Add degree to our data set too
     DF_TEMP$degree <- degree(plot_graph)
@@ -109,71 +135,77 @@ for (nP in unique(DF_NEW$numPlants)){
     zippin[,1] <- DF_TEMP$X
     zippin[,2] <- DF_TEMP$Y
     
-    
-    
    # plot(plot_graph,layout = zippin,vertex.size = 1, vertex.label = NA,edge.arrow.size = 0.1,edge.color="red",edge.curved=0,vertex.color="black",edge.width=2,frame=T)
+    
+    print("con")
+    print(contador)
+    if (contador ==0){
+    plot(plot_graph,layout = zippin,vertex.size = 1, vertex.label = NA,edge.arrow.size = 0.1,
+         edge.color="red",edge.curved=0,vertex.color="black",edge.width=2,frame=T,
+         main = paste("numPlants", nP, "coffee maturation", pC, "rep", rP))
+   }
+    contador <- contador+1
     
     DF_TEMP <- DF_TEMP %>%
       select(Rep, numPlants, porcionCosecha)
     
-    DF_TEMP$N_REDES <- numeroRedes
+    DF_TEMP$N_REDES <- 2* numeroRedes/DF_TEMP$numPlants
     DF_TEMP$T_REDES <- tamanioRedes
     DF_TEMP$MAX_T_REDES <-  maxTamRedes
+    DF_TEMP$MUL_N_T <- DF_TEMP$N_REDES*DF_TEMP$T_REDES
+    
     
     DF_POST <- rbind(DF_POST, DF_TEMP)
     
     }
   }
+    }
 }
-}
+
+
+
 
 DF_POST <- DF_POST %>%
   group_by(Rep, numPlants, porcionCosecha)%>%
   summarise_all(mean)
 
-
 #falta afinar este resultado!! en realidad el punto es que por cada repeticin haga la multi y desues sacamos des estandarss
 
 DF_POST_RES <- DF_POST %>%
   group_by(numPlants, porcionCosecha)%>%
+  select(numPlants, porcionCosecha, N_REDES, T_REDES, MUL_N_T)%>%
   summarise_all(list(mean = ~mean(.), sd = ~sd(.)))
 
 
-DF_POST_RES$N_REDES_NOR <- DF_POST_RES$N_REDES/DF_POST_RES$numPlants*100 
+#DF_POST_RES$N_REDES_NOR <- DF_POST_RES$N_REDES/DF_POST_RES$numPlants*100 
 
-DF_POST_RES$MUL_N_T <- DF_POST_RES$N_REDES_NOR*DF_POST_RES$T_REDES
+DF_POST_RES$porcionCosecha[DF_POST_RES$porcionCosecha =="0.5"] <- "Asynchronous"
+DF_POST_RES$porcionCosecha[DF_POST_RES$porcionCosecha =="1"] <- "Synchronous"
 
+melt_mean <- DF_POST_RES %>%
+  select(numPlants, porcionCosecha, N_REDES_mean, T_REDES_mean, MUL_N_T_mean)%>%
+  melt(id = c("numPlants","porcionCosecha"), value.name = "value_mean", variable.name = "variable_mean") 
 
-
-FIG_REDES <- DF_POST_RES %>% 
-  ggplot(aes(x= numPlants))+
-  geom_line(size= 1.5, aes(y= MUL_N_T, color= as.character(porcionCosecha)))+
-  geom_ribbon(aes( 
-    ymin=AverageRust-SD_Rust, ymax=AverageRust+SD_Rust),alpha=0.5) +
-
-#+
-
+melt_sd <- DF_POST_RES %>%
+  select(numPlants, porcionCosecha, N_REDES_sd, T_REDES_sd, MUL_N_T_sd)%>%
+  melt(id = c("numPlants","porcionCosecha"), value.name = "value_sd", variable.name = "variable_sd") 
 
 
+melt_DF_POST_RES <- cbind(melt_mean, melt_sd[,c(3:4)])
 
-  #geom_point(aes(y= AverageRust, color= as.character(porcionCosecha)), size= 2)+
-  geom_ribbon(aes( 
-    ymin=AverageRust-SD_Rust, ymax=AverageRust+SD_Rust),alpha=0.5) +
-  ggtitle("")+
-  facet_wrap(~ numPlants, nrow =1)+
-  scale_fill_manual(values = groupColors4)+
-  scale_color_manual(values = groupColors4)+
-  theme_bw()+
+
+FIG_REDES <- melt_DF_POST_RES %>% 
+  ggplot(aes(x= numPlants, y= value_mean))+
+  geom_point(size= 1.5, aes(color = porcionCosecha))+
+  geom_line(aes(color = porcionCosecha, linetype = porcionCosecha))+
+  geom_errorbar(aes( 
+    ymin=value_mean-value_sd, ymax=value_mean+value_sd, color = porcionCosecha)) +
+  scale_color_manual(values = groupColors2)+
+  scale_fill_manual(values = groupColors2)+
+  facet_wrap(~variable_mean, scales = "free_y")+
+  theme_bw() +
   theme(text = element_text(size = 25))+
-  theme(strip.background = element_rect(fill = "white"))+ 
-  #theme(legend.position = "none")
-  labs(x= "Time", y= "Average Rust", color= "Coffee Maturation", fill= "Coffee Maturation")
-ggsave(FIG_SLI_time,filename=paste("../../output/graficas/SUP_FIG/", "rust_time_abs", ".png", sep=""),  height = 8, width = 22) # ID will be the unique identifier. and change the extension from .png to whatever you like (eps, pdf etc)
-
-
-
-
-
+  theme(strip.background = element_rect(fill = "white"))
 
 
 
